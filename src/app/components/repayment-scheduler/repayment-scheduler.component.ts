88fcd9436274bf68d076be678e5709b44eb3c893
Pascal Bossert <pascal.bossert@gmail.com>
2025-04-13 21:47:29 +0200

    adjust styling to keep input fixed while allowing table to scroll

--- src/app/components/repayment-scheduler/repayment-scheduler.component.ts
+++ src/app/components/repayment-scheduler/repayment-scheduler.component.ts
@@ -1,12 +1,14 @@
 import {Component} from '@angular/core';
 import {LoanFormComponent} from '@components/repayment-scheduler/loan-form/loan-form.component';
 import {RepaymentTableComponent} from '@components/repayment-scheduler/repayment-table/repayment-table.component';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-repayment-scheduler',
   imports: [
     LoanFormComponent,
-    RepaymentTableComponent
+    RepaymentTableComponent,
+    MatCardModule
   ],
   templateUrl: './repayment-scheduler.component.html',
   styleUrl: './repayment-scheduler.component.scss'
